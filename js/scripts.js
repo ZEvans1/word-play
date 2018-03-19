16a33bb88b607cb313d02a02e2e98d595be6ff58
ZEvans1 <zte.zachary@gmail.com>
2018-03-19 13:38:25 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     var sentences = $("input#user").val();
     //set up value with the user input
     var splits= sentences.split(' ', );
-    //using .split to slit the strung into an array
+    //using .split to slit the string into an array
     var newArray = [];
     //creating new empty array
     splits.forEach(function(split) {
       if (split.length > 3) {
-        // cpecifying the creteria > 3 characters inlength
+        // specifying the criteria > 3 characters in length
         newArray.push(split);
-        // splitting out the characters crateris >3
+        // splitting out the characters >3
       }
     })
     alert(newArray);
